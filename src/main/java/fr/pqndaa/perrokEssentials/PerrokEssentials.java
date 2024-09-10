5ccff37f96911fd1d6101c48c08b7090584ec52c
PQNDAAA <gayvallet.dylan@gmail.com>
2024-09-10 13:13:12 +0200

    First commit

---
+++ src/main/java/fr/pqndaa/perrokEssentials/PerrokEssentials.java
@@ -0,0 +1,17 @@
+package fr.pqndaa.perrokEssentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PerrokEssentials extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
